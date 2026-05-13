c77bf159dce3b262f836cdcdca6e8de2adb9cd7c
mariam-akhter <mariamj098ahan@gmail.com>
2026-05-13 22:36:19 +0600

    unit testing on monitoring,research paper and urls

--- library/tests/test_reports_monitoring.py
+++ library/tests/test_reports_monitoring.py
@@ -1,22 +1,45 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class ReportsMonitoringTest(TestCase):
 
+    def setUp(self):
+
+        User = get_user_model()
+
+        self.user = User.objects.create_user(
+            username="adminuser",
+            email="admin@gmail.com",
+            password="testpass123"
+        )
+
+        self.user.is_staff = True
+        self.user.is_superuser = True
+        self.user.save()
+
+        self.client.login(
+            username="adminuser",
+            password="testpass123"
+        )
+
     def test_reports_analytics_page_opens(self):
+
         response = self.client.get(reverse("reports_analytics"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "library/reports_analytics.html")
 
     def test_system_monitoring_page_opens(self):
+
         response = self.client.get(reverse("system_monitoring"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "library/system_monitoring.html")
 
     def test_premium_content_page_opens(self):
+
         response = self.client.get(reverse("premium_content"))
 
         self.assertEqual(response.status_code, 200)
--- library/tests/test_research_papers.py
+++ library/tests/test_research_papers.py
@@ -1,22 +1,45 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class ResearchPaperTest(TestCase):
 
+    def setUp(self):
+
+        User = get_user_model()
+
+        self.user = User.objects.create_user(
+            username="researchuser",
+            email="research@gmail.com",
+            password="testpass123"
+        )
+
+        self.user.is_staff = True
+        self.user.is_superuser = True
+        self.user.save()
+
+        self.client.login(
+            username="researchuser",
+            password="testpass123"
+        )
+
     def test_research_papers_page_opens(self):
+
         response = self.client.get(reverse("research_papers"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "library/research_papers.html")
 
     def test_upload_research_paper_page_opens(self):
+
         response = self.client.get(reverse("upload_research_paper"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "library/upload_research_paper.html")
 
     def test_manage_research_papers_page_opens(self):
+
         response = self.client.get(reverse("manage_research_papers"))
 
         self.assertEqual(response.status_code, 200)
